479ad4f5aa08a117703680c2d48215efb8229e80
tomoutsuki <outsuki.tom@gmail.com>
2024-05-17 15:31:10 -0300

    JavaScript commitment

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let pressedArray = [];
 let correctArray = [];
 
-const green_bt  = getElementById("green_bt");
-const yellow_bt = getElementById("yellow_bt");
-const red_bt    = getElementById("red_bt");
-const blue_bt   = getElementById("blue_bt");
+const green_bt  = document.getElementById("green_bt");
+const yellow_bt = document.getElementById("yellow_bt");
+const red_bt    = document.getElementById("red_bt");
+const blue_bt   = document.getElementById("blue_bt");
 
-// Green = 0; Yellow = 1;
-// Red = 2; Blue = 3;
+// Green = 0; #26bd50
+//Yellow = 1; #f4eb0f
+// Red = 2; #dd3620
+//Blue = 3; #4757ec
+
 
 const pressButton = (color) => {
     pressedArray.push(color);
@@ -29,10 +32,38 @@ const judgePress = () => {
     }
 }
 
+const playButtons = async () => {
+    for (element of correctArray) {
+        
+        switch(element) {
+            case 0:
+                green_bt.style.backgroundColor = "#50fa35";
+                break;
+            case 1:
+                yellow_bt.style.backgroundColor = "#fafe58";
+                break;
+            case 2:
+                red_bt.style.backgroundColor = "#ff7347";
+                break;
+            case 3:
+                blue_bt.style.backgroundColor = "#6edeed";
+                break;
+        }
+        await sleep(1000);
+
+    }
+}
+
 const gameOver = () => {
     console.log("Game Over");
 }
 
 const Start = () => {
     newElement();
+}
+
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
