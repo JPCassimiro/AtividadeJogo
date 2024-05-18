1e6a97b9b94e5957c20d6726cb5364b4c11d89c2
tomoutsuki <outsuki.tom@gmail.com>
2024-05-17 23:07:25 -0300

    Some changes

--- script.js
+++ script.js
@@ -6,20 +6,36 @@ const yellow_bt = document.getElementById("yellow_bt");
 const red_bt    = document.getElementById("red_bt");
 const blue_bt   = document.getElementById("blue_bt");
 
+const buttonSound = new Audio('./sound/button.mp3');
+const gameoverSound = new Audio('./sound/gameover.mp3');
+const ponSound = [
+    new Audio('./sound/pon1.mp3'),
+    new Audio('./sound/pon2.mp3'),
+    new Audio('./sound/pon3.mp3'),
+    new Audio('./sound/pon4.mp3'),
+];
+const winSound = new Audio('./sound/win.mp3');
+const wrongSound = new Audio('./sound/wrong.mp3');
+
 // Green = 0; #26bd50
 //Yellow = 1; #f4eb0f
 // Red = 2; #dd3620
 //Blue = 3; #4757ec
 
 
-const pressButton = (color) => {
-    pressedArray.push(color);
-    judgePress();
+const pressButton = async (button) => {
+    pressedArray.push(button);
+    
+    turnOnButton(button);
+    await sleep(1000);
+    turnOffButton(button);
+    //button.style.backgroundColor = "#ff0000";
+    //judgePress();
 }
 
 const newElement = ()=> {
     let newNumber = Math.floor(Math.random() * 4);
-    newElement.push();
+    console.log(newNumber);
 }
 
 const judgePress = () => {
@@ -32,23 +48,52 @@ const judgePress = () => {
     }
 }
 
+const turnOnButton = (number) => {
+    switch(number) {
+
+        case 0:
+            green_bt.style.backgroundColor = "#50fa35";
+            break;
+
+        case 1:
+            yellow_bt.style.backgroundColor = "#fafe58";
+            break;
+
+        case 2:
+            red_bt.style.backgroundColor = "#ff7347";
+            break;
+
+        case 3:
+            blue_bt.style.backgroundColor = "#6edeed";
+            break;
+    }
+}
+
+const turnOffButton = (number) => {
+    switch(number) {
+        case 0:
+            green_bt.style.backgroundColor = "#26bd50";
+            break;
+
+        case 1:
+            yellow_bt.style.backgroundColor = "#f4eb0f";
+            break;
+
+        case 2:
+            red_bt.style.backgroundColor = "#dd3620";
+            break;
+
+        case 3:
+            blue_bt.style.backgroundColor = "#4757ec";
+            break;
+    }
+}
+
 const playButtons = async () => {
     for (element of correctArray) {
-        
-        switch(element) {
-            case 0:
-                green_bt.style.backgroundColor = "#50fa35";
-                break;
-            case 1:
-                yellow_bt.style.backgroundColor = "#fafe58";
-                break;
-            case 2:
-                red_bt.style.backgroundColor = "#ff7347";
-                break;
-            case 3:
-                blue_bt.style.backgroundColor = "#6edeed";
-                break;
-        }
+        let button = getButton(element);
+        let color = parseInt(button.style.backgroundColor);
+        let newColor = (color + parseInt('#000000')).toString(16);
         await sleep(1000);
 
     }
@@ -60,6 +105,7 @@ const gameOver = () => {
 
 const Start = () => {
     newElement();
+    //buttonSound.play();
 }
 
 
